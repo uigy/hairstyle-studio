7b34e2b3d405ed709d92456afac9da9e42429ac3
uigy <uigy@pm.me>
2021-02-15 17:39:56 +0100

    updated

--- src/components/Categories/index.js
+++
@@ -1,8 +0,0 @@
-import About from "./About";
-import Offer from "./Offer";
-import Appointment from "./Appointment";
-import Contact from "./Contact";
-
-const Categories = [About, Offer, Appointment, Contact];
-
-export default Categories;
--- src/components/Nav/NavIcons/index.js
+++
@@ -1,8 +0,0 @@
-import HomeIcon from "./HomeIcon";
-import OfferIcon from "./OfferIcon";
-import AppointmentIcon from "./AppointmentIcon";
-import ContactIcon from "./ContactIcon";
-
-const NavIcons = [HomeIcon, OfferIcon, AppointmentIcon, ContactIcon];
-
-export default NavIcons;
